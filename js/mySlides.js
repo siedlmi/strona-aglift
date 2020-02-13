dd8b69cf54b7fa2b2a9763cf2cbfe3dda5d1c4fd
siedlmi <30671703+siedlmi@users.noreply.github.com>
2020-02-13 07:48:22 +0100

    Update mySlides.js

--- js/mySlides.js
+++ js/mySlides.js
@@ -1,30 +1,19 @@
-  var slideIndex = 1;
-  showSlides(slideIndex);
+// Slideshows
+var slideIndex = 1;
+var x = document.getElementsByClassName("mySlides");
+   
+function plusDivs(n) {
+  slideIndex = slideIndex + n;
+  showDivs(slideIndex);
+}
 
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+function showDivs(n) {
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
   }
+  x[slideIndex-1].style.display = "block";  
+}
 
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+showDivs(1)
