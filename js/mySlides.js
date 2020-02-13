6d94ce5d3e7c0e87116b67632e42782f39fadbe1
siedlmi <30671703+siedlmi@users.noreply.github.com>
2020-02-13 08:25:52 +0100

    Update mySlides.js

--- js/mySlides.js
+++ js/mySlides.js
@@ -13,7 +13,7 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+ //x[slideIndex-1].style.display = "block";  
 }
 
 showDivs(0)
